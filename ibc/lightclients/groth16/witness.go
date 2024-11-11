46d7e448647b2a8e8c2298c815e2fa6e73488f0d
Nina Barbakadze <barbakadzeninaa@gmail.com>
2024-11-11 15:50:34 +0100

    refactor: cleanup witness

--- ibc/lightclients/groth16/witness.go
+++ ibc/lightclients/groth16/witness.go
@@ -8,17 +8,17 @@ import (
 )
 
 type PublicWitness struct {
-	TrustedHeight int64 // Provided by the relayer/user
+	TrustedHeight             int64  // Provided by the relayer/user
 	TrustedCelestiaHeaderHash []byte // Provided by the ZK IBC Client
-	TrustedRollupStateRoot []byte // Provided by the ZK IBC Client
-	NewHeight int64 // Provided by the relayer/user
-	NewRollupStateRoot []byte // Provided by the relayer/user
-	NewCelestiaHeaderHash []byte // Provided by Celestia State Machine
-	CodeCommitment []byte // Provided during initialization of the IBC Client
-	GenesisStateRoot []byte // Provided during initialization of the IBC Client
+	TrustedRollupStateRoot    []byte // Provided by the ZK IBC Client
+	NewHeight                 int64  // Provided by the relayer/user
+	NewRollupStateRoot        []byte // Provided by the relayer/user
+	NewCelestiaHeaderHash     []byte // Provided by Celestia State Machine
+	CodeCommitment            []byte // Provided during initialization of the IBC Client
+	GenesisStateRoot          []byte // Provided during initialization of the IBC Client
 }
 
-func (p PublicWitness) GenerateStateTransitionPublicWitness(trustedStateRoot []byte) (witness.Witness, error) {
+func (p PublicWitness) Generate() (witness.Witness, error) {
 	w, err := witness.New(ecc.BN254.ScalarField())
 	if err != nil {
 		return nil, err
@@ -26,16 +26,9 @@ func (p PublicWitness) GenerateStateTransitionPublicWitness(trustedStateRoot []b
 
 	numInputs := 5
 
-	trustedCelestiaHeaderHash := 
-	newCelestiaHeaderHash := 
-
 	// Create a channel to send values to the witness
 	values := make(chan any, numInputs)
-	values <- p.TrustedHeight
-	values <- trustedStateRoot
-	values <- p.NewHeight
-	values <- h.NewStateRoot
-	values <- h.DataRoots
+	values <- p
 	close(values)
 
 	err = w.Fill(numInputs, 0, values)
