e1f620eaf457349a3e9fed0b830d735eb2da94b4
Callum Waters <cmwaters19@gmail.com>
2024-11-25 14:48:30 +0100

    set permissions on the p2p key in celestia bridge

--- testing/files/celestia-bridge/entrypoint.sh
+++ testing/files/celestia-bridge/entrypoint.sh
@@ -3,6 +3,9 @@
 # Copy config files from testing/files/celestia-validator to .tmp directory
 cp -r /testapp_files/* /home/celestia/
 
+# Set permissions for keys
+chmod 600 keys/OAZHALLLMV4Q 
+
 echo "$@"
 
 exec $@
